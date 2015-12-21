d7ad50d456f66ff534a9acd0aa09e755ae3fee68
SillyFreak <sillyfreak.ck@gmail.com>
2015-12-21 12:25:57 +0100

    rename main.py to __main__.py, move into package

---
+++ hedgehog_light/__main__.py
@@ -0,0 +1,25 @@
+import sys
+from hedgehog_light.flasher import Flasher, FlasherException
+
+argv = sys.argv[1:]
+
+flasher = Flasher()
+try:
+    flasher.init_chip()
+    version, cmds = flasher.cmd_get()
+    id_ = flasher.cmd_get_id()
+
+    print("Bootloader version:", hex(version))
+    print("Commands:", [hex(cmd) for cmd in cmds])
+    print("Extended erase:", 0x44 in cmds)
+    print("ID:", hex(id_))
+
+    bin_ = open(argv[0], 'rb').read()
+
+    flasher.cmd_extended_erase_memory(mode='global')
+    flasher.write_memory(bin_)
+    verify = flasher.read_memory(len(bin_))
+    if bin_ != verify:
+        raise FlasherException('Verify failed')
+finally:
+    flasher.release_chip()
