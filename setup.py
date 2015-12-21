0741cd665592dc18a7880622fad83aec92093907
SillyFreak <sillyfreak.ck@gmail.com>
2015-12-21 12:09:31 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+import hedgehog_light
+
+setup(
+    name="stm32flasher",
+    description="STM32 USART Flasher",
+    version=hedgehog_light.__version__,
+    license="AGPLv3",
+    url="https://github.com/PRIArobotics/STM32Flasher",
+    author="Clemens Koza",
+    author_email="koza@pria.at",
+
+    packages=['hedgehog_light'],
+    install_requires=['pyserial>=2.7'],
+)
+
